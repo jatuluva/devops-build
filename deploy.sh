124f3f503fa767b0b87793cd5076d3649819f81c
jatuluva <154571049+jatuluva@users.noreply.github.com>
2024-08-31 11:58:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ if [[ $GIT_BRANCH == "dev" ]]; then
     # Build your project
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u jatuluva -p $DOCKER_PASSWORD
-    docker tag test jatuluva/dev
+    docker login -u jatuluva -p dckr_pat_z6YabM32MsYP3CKKQxtcy3b9RFU
+    docker tag capstonenginx jatuluva/dev
     docker push jatuluva/dev
 
 elif [[ $GIT_BRANCH == "main" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u jatuluva -p $DOCKER_PASSWORD
-    docker tag test jatuluva/prod 
+    docker login -u jatuluva -p dckr_pat_z6YabM32MsYP3CKKQxtcy3b9RFU
+    docker tag capstonenginx jatuluva/prod 
     docker push jatuluva/prod
 fi
