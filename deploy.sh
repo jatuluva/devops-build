38e20ea2d4ee8a5144a0fa95e9f31b398bb3bea0
jatuluva <154571049+jatuluva@users.noreply.github.com>
2024-08-31 12:45:18 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-if [[ $GIT_BRANCH == "origin/dev" ]]; then
+if [[ $GIT_BRANCH == "dev" ]]; then
     # Build your guviproject
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u jatuluva -p $DOCKER_PASSWORD
-    docker tag test jatuluva/dev
+    docker login -u jatuluva -p dckr_pat_HByDTtS4kHPPZwPRib3bYV8huIA
+    docker tag capstonenginx jatuluva/dev
     docker push jatuluva/dev
 
-elif [[ $GIT_BRANCH == "origin/main" ]]; then
+elif [[ $GIT_BRANCH == "main" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u jatuluva -p $DOCKER_PASSWORD
-    docker tag test jatuluva/prod 
+    docker login -u jatuluva -p dckr_pat_HByDTtS4kHPPZwPRib3bYV8huIA
+    docker tag capstonenginx jatuluva/prod 
     docker push jatuluva/prod
 fi
