b51451ece483c9ab10f706e8fa7dbbe77a8a5e22
jatuluva <154571049+jatuluva@users.noreply.github.com>
2024-08-31 10:59:38 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     # Build your project
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u jatuluva8888 -p $DOCKER_PASSWORD
-    docker tag test jatuluva8888/dev
-    docker push jatuluva8888/dev
+    docker login -u jatuluva -p $DOCKER_PASSWORD
+    docker tag test jatuluva/dev
+    docker push jatuluva/dev
 
 elif [[ $GIT_BRANCH == "origin/main" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u jatuluva8888 -p $DOCKER_PASSWORD
-    docker tag test jatuluva8888/prod 
-    docker push jatuluva8888/prod
+    docker login -u jatuluva -p $DOCKER_PASSWORD
+    docker tag test jatuluva/prod 
+    docker push jatuluva/prod
 fi
